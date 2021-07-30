98036e8910624c8fd8f9eb2e3b130f5d111a8fcf
JunyaKosaka <kosakajunya@gmail.com>
2021-07-30 10:05:58 +0900

    add ac4.cpp

---
+++ virtual_contest/ac4.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+#define rep(i,n) for (int i=0; i<(n); ++i)
+#define sz(x) int(x.size())
+#define show(x) {for(auto i:x){cout << i << " ";} cout << endl;}
+#define isin(x,l,r) ((l)<=(x) && (x)<(r))
+using namespace std;
+using ll=long long;
+using pii=pair<int,int>;
+using pll=pair<ll,ll>;
+using tii=tuple<int, int, int>;
+using tll=tuple<ll, ll, ll>;
+template<typename T>bool chmin(T&x,const T&y) {if(x>y){x=y;return true;} else return false;}
+template<typename T>bool chmax(T&x,const T&y) {if(x<y){x=y;return true;} else return false;}
+
+int main() {
+    double T, L, X, Y;
+    cin >> T >> L >> X >> Y;
+    double PI = acos(-1);
+    int Q;
+    cin >> Q;
+    rep(i, Q) {
+        double E;
+        cin >> E;
+        double x = 0, y, z;
+        y = L / 2 * cos(- PI / 2 - PI * 2 * E / T);
+        z = L / 2 + L / 2 * sin(- PI / 2 - PI * 2 * E / T);
+        double ans = atan2(z, sqrt((X-x) * (X-x) + (Y-y) * (Y-y))) * 180 / PI;
+        printf("%.10f\n", ans);
+    }
+    return 0;
+}
